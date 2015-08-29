e67c9709882df7c70e59b73bd3d7510e676af09c
Mike Higginbottom <mike@mikehigginbottom.com>
2015-08-29 18:44:25 +0100

    Align header line for ex1-3

--- ex1-3.c
+++ ex1-3.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	float lower = 0, upper = 300, step = 20;
 	float fahr = lower, celsius;
-	printf("F     C\n");
+	printf("%3s %6s\n", "F", "C");
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%3.0f %6.1f\n", fahr, celsius);
