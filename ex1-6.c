bb240da9a476de5fa2346bc6389a4e84cee54204
Mike Higginbottom <mike@mikehigginbottom.com>
2015-08-29 21:42:05 +0100

    Added clarifying parentheses round while loop condition. Avoids compiler warning.

--- ex1-6.c
+++ ex1-6.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int result;
 	printf("Press a key. You may need to press ENTER afterwards depending on your terminal emulator's buffering method.\n");
-	while (result = (getchar() != EOF))
+	while ((result = (getchar() != EOF)))
 		printf("Not EOF %d\n", result);
 	printf("EOF %d\n", result);
 	return 0;
