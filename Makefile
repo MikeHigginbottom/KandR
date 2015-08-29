5603feceb32b612dc35bb523982ebe814e6b6e7e
Mike Higginbottom <mike@mikehigginbottom.com>
2015-08-29 18:07:11 +0100

    Added ex1-3

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 override CFLAGS += -g -Wall
 .PHONY: all
-all: ex1-1 ex1-2
+all: ex1-1 ex1-2 ex1-3
 
 .PHONY: clean
 clean:
 	-rm -f *.o \
-	ex1-1 ex1-2
+	ex1-1 ex1-2 ex1-3
---
+++ ex1-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float lower = 0, upper = 300, step = 20;
+	float fahr = lower, celsius;
+	printf("F     C\n");
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr += step;
+	}
+	return 0;
+}
